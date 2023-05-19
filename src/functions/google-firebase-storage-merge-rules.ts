2cbcac6f65283f1244df8e5b39f2ae28d956464d
Flo <florent.vouin@gmail.com>
2023-05-19 10:50:24 +0200

    ✨ Implement the GoogleFirebaseStorageMergeRules function

---
+++ src/functions/google-firebase-storage-merge-rules.ts
@@ -0,0 +1,85 @@
+import { CliCommand } from '@causa/cli';
+import { WorkspaceContext, WorkspaceFunction } from '@causa/workspace';
+import { InfrastructureProcessor } from '@causa/workspace-core';
+import { firebaseStorageCommandDefinition } from '../cli/index.js';
+import { GoogleConfiguration } from '../configurations/index.js';
+import { mergeFirebaseRulesFiles } from '../firebase/index.js';
+
+/**
+ * The default location where the security rules file for Firebase Storage will be written.
+ */
+const DEFAULT_FIREBASE_STORAGE_SECURITY_RULE_FILE = 'storage.rules';
+
+/**
+ * The return value of {@link GoogleFirebaseStorageMergeRules}.
+ */
+type GoogleFirebaseStorageMergeRulesResult = {
+  /**
+   * A configuration with `google.firebaseStorage.securityRuleFile` set to the path to the merged rules file.
+   */
+  configuration: GoogleConfiguration;
+
+  /**
+   * The path to the merged rules file.
+   */
+  securityRuleFile: string;
+};
+
+/**
+ * A function that merges all the Firebase Storage security rules found in the workspace into a single file, which can
+ * be used for the emulator and as the configuration of the Firebase Storage service.
+ * The `google.firebaseStorage.securityRuleFiles` configuration can be used to pass a list of glob patterns to find
+ * security rule files in the workspace.
+ * The `google.firebaseStorage.securityRuleFile` configuration can be used to specify the output location of the merged
+ * rules file.
+ * Returns a configuration with `google.firebaseStorage.securityRuleFile` set, such that the function can be used as a
+ * processor.
+ */
+@CliCommand({
+  parent: firebaseStorageCommandDefinition,
+  name: 'mergeRules',
+  description: `Merge Firebase Storage security rules into a single file.
+Input files are looked for in the workspace using the globs defined in google.firebaseStorage.securityRulesFiles.`,
+  summary: 'Merge Firebase Storage security rules into a single file.',
+  outputFn: ({ securityRuleFile }) => console.log(securityRuleFile),
+})
+export class GoogleFirebaseStorageMergeRules
+  extends WorkspaceFunction<Promise<GoogleFirebaseStorageMergeRulesResult>>
+  implements InfrastructureProcessor
+{
+  async _call(
+    context: WorkspaceContext,
+  ): Promise<GoogleFirebaseStorageMergeRulesResult> {
+    const googleConf = context.asConfiguration<GoogleConfiguration>();
+    let securityRuleFile =
+      googleConf.get('google.firebaseStorage.securityRuleFile') ??
+      DEFAULT_FIREBASE_STORAGE_SECURITY_RULE_FILE;
+    const globs =
+      googleConf.get('google.firebaseStorage.securityRuleFiles') ?? [];
+
+    securityRuleFile = await mergeFirebaseRulesFiles(
+      'Firebase Storage',
+      globs,
+      (rules) =>
+        `rules_version = '2';
+
+service firebase.storage {
+  match /b/{bucket}/o {
+${rules}
+  }
+}
+`,
+      securityRuleFile,
+      context,
+    );
+
+    return {
+      configuration: { google: { firebaseStorage: { securityRuleFile } } },
+      securityRuleFile,
+    };
+  }
+
+  _supports(): boolean {
+    return true;
+  }
+}
