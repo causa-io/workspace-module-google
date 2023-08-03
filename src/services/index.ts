435719fe2e919c718f179482e7bff3b2aed82848
Flo <florent.vouin@gmail.com>
2023-08-03 16:34:31 +0200

    ✨ Implement the IamService

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,7 @@ export { FirebaseAppService, FirebaseAppType } from './firebase-app.js';
 export { FirebaseEmulatorService } from './firebase-emulator.js';
 export { GcloudEmulatorService } from './gcloud-emulator.js';
 export { GoogleApisService } from './google-apis.js';
+export { IamService } from './iam.js';
 export { GoogleSecretManagerService } from './secret-manager.js';
 export * from './storage.errors.js';
 export { CloudStorageService } from './storage.js';
