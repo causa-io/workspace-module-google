e076a20e148f47b3a118bbffa699d08e971a4b02
Flo <florent.vouin@gmail.com>
2026-02-06 11:58:33 +0100

    ⚡ Defer call for EventTopicBrokerDeleteTopicForPubSub

--- src/functions/event-topic/broker-delete-topic-pubsub.ts
+++ src/functions/event-topic/broker-delete-topic-pubsub.ts
@@ -3,8 +3,7 @@ import {
   EventTopicBrokerDeleteTopic,
   type EventsConfiguration,
 } from '@causa/workspace-core';
-import { grpc } from 'google-gax';
-import { PubSubService } from '../../services/index.js';
+import { callDeferred } from '../utils.js';
 
 /**
  * Implements {@link EventTopicBrokerDeleteTopic} for Pub/Sub.
@@ -12,20 +11,7 @@ import { PubSubService } from '../../services/index.js';
  */
 export class EventTopicBrokerDeleteTopicForPubSub extends EventTopicBrokerDeleteTopic {
   async _call(context: WorkspaceContext): Promise<void> {
-    context.logger.info(`📫 Deleting Pub/Sub topic '${this.id}'.`);
-
-    try {
-      await context.service(PubSubService).pubSub.topic(this.id).delete();
-    } catch (error: any) {
-      if (error.code === grpc.status.NOT_FOUND) {
-        context.logger.warn(
-          `⚠️ Pub/Sub topic to delete '${this.id}' does not exist. It might have already been deleted.`,
-        );
-        return;
-      }
-
-      throw error;
-    }
+    return await callDeferred(this, context, import.meta.url);
   }
 
   _supports(context: WorkspaceContext): boolean {
