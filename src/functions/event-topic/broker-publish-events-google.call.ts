e323675b3b38845519a3e4102970ad8ca52228a9
Flo <florent.vouin@gmail.com>
2026-02-06 11:58:46 +0100

    ⚡ Defer call for EventTopicBrokerPublishEventsForGoogle

---
+++ src/functions/event-topic/broker-publish-events-google.call.ts
@@ -0,0 +1,62 @@
+import type { WorkspaceContext } from '@causa/workspace';
+import {
+  type BackfillEventsSource,
+  JsonFilesEventSource,
+} from '@causa/workspace-core';
+import {
+  BigQueryEventsSource,
+  PubSubBackfillEventPublisher,
+} from '../../backfilling/index.js';
+import type { GoogleConfiguration } from '../../index.js';
+import type { EventTopicBrokerPublishEventsForGoogle } from './broker-publish-events-google.js';
+
+async function createSource(
+  self: EventTopicBrokerPublishEventsForGoogle,
+  context: WorkspaceContext,
+): Promise<BackfillEventsSource> {
+  if (!self.source) {
+    const googleConf = context.asConfiguration<GoogleConfiguration>();
+    const projectId = googleConf.getOrThrow('google.project');
+    const rawEventsDatasetId = googleConf.get(
+      'google.pubSub.bigQueryStorage.rawEventsDatasetId',
+    );
+    if (!rawEventsDatasetId) {
+      throw new Error(
+        'Cannot use the default event source because BigQuery storage is not configured.',
+      );
+    }
+
+    const tableName = self.eventTopic.replace(/[-\.]/g, '_');
+    const tableId = `${projectId}.${rawEventsDatasetId}.${tableName}`;
+    return new BigQueryEventsSource(context, tableId);
+  }
+
+  const bqSource = await BigQueryEventsSource.fromSourceAndFilter(
+    context,
+    self.source,
+    self.filter,
+  );
+  if (bqSource) {
+    return bqSource;
+  }
+
+  const jsonFilesSource = await JsonFilesEventSource.fromSourceAndFilter(
+    context,
+    self.source,
+    self.filter,
+  );
+  if (jsonFilesSource) {
+    return jsonFilesSource;
+  }
+
+  throw new Error(`The event source '${self.source}' is not supported.`);
+}
+
+export default async function call(
+  this: EventTopicBrokerPublishEventsForGoogle,
+  context: WorkspaceContext,
+): Promise<void> {
+  const source = await createSource(this, context);
+  const publisher = new PubSubBackfillEventPublisher(context, this.topicId);
+  await publisher.publishFromSource(source);
+}
--- src/functions/event-topic/broker-publish-events-google.ts
+++ src/functions/event-topic/broker-publish-events-google.ts
@@ -1,72 +1,17 @@
 import { WorkspaceContext } from '@causa/workspace';
 import {
-  type BackfillEventsSource,
   EventTopicBrokerPublishEvents,
   type EventsConfiguration,
-  JsonFilesEventSource,
 } from '@causa/workspace-core';
-import {
-  BigQueryEventsSource,
-  PubSubBackfillEventPublisher,
-} from '../../backfilling/index.js';
-import type { GoogleConfiguration } from '../../index.js';
+import { callDeferred } from '../utils.js';
 
 /**
  * Implements {@link EventTopicBrokerPublishEvents} for a Google / GCP stack.
  * The supported message broker is Pub/Sub.
  */
 export class EventTopicBrokerPublishEventsForGoogle extends EventTopicBrokerPublishEvents {
-  /**
-   * Tries to create a {@link BackfillEventsSource} from the provided `source` and `filter`.
-   *
-   * @param context The {@link WorkspaceContext}.
-   * @returns The {@link BackfillEventsSource} to use for backfilling.
-   */
-  private async createSource(
-    context: WorkspaceContext,
-  ): Promise<BackfillEventsSource> {
-    if (!this.source) {
-      const googleConf = context.asConfiguration<GoogleConfiguration>();
-      const projectId = googleConf.getOrThrow('google.project');
-      const rawEventsDatasetId = googleConf.get(
-        'google.pubSub.bigQueryStorage.rawEventsDatasetId',
-      );
-      if (!rawEventsDatasetId) {
-        throw new Error(
-          'Cannot use the default event source because BigQuery storage is not configured.',
-        );
-      }
-
-      const tableName = this.eventTopic.replace(/[-\.]/g, '_');
-      const tableId = `${projectId}.${rawEventsDatasetId}.${tableName}`;
-      return new BigQueryEventsSource(context, tableId);
-    }
-
-    const bqSource = await BigQueryEventsSource.fromSourceAndFilter(
-      context,
-      this.source,
-      this.filter,
-    );
-    if (bqSource) {
-      return bqSource;
-    }
-
-    const jsonFilesSource = await JsonFilesEventSource.fromSourceAndFilter(
-      context,
-      this.source,
-      this.filter,
-    );
-    if (jsonFilesSource) {
-      return jsonFilesSource;
-    }
-
-    throw new Error(`The event source '${this.source}' is not supported.`);
-  }
-
   async _call(context: WorkspaceContext): Promise<void> {
-    const source = await this.createSource(context);
-    const publisher = new PubSubBackfillEventPublisher(context, this.topicId);
-    await publisher.publishFromSource(source);
+    return await callDeferred(this, context, import.meta.url);
   }
 
   _supports(context: WorkspaceContext): boolean {
