3c498fd0d7ef846cd09028bd14b88514a5d5e9fa
Flo <florent.vouin@gmail.com>
2026-04-21 15:18:29 +0200

    ♻️ Handle source async iterable breaking change in EventTopicBrokerPublishEventsForGoogle

--- src/functions/event-topic/broker-publish-events-google.call.ts
+++ src/functions/event-topic/broker-publish-events-google.call.ts
@@ -1,62 +1,62 @@
 import type { WorkspaceContext } from '@causa/workspace';
-import {
-  type BackfillEventsSource,
-  JsonFilesEventSource,
-} from '@causa/workspace-core/backfill';
-import {
-  BigQueryEventsSource,
-  PubSubBackfillEventPublisher,
-} from '../../backfilling/index.js';
-import type { GoogleConfiguration } from '../../index.js';
+import type { PublishOptions } from '@google-cloud/pubsub';
+import { FlowControlledPublisher } from '@google-cloud/pubsub/build/src/publisher/flow-publisher.js';
+import { PubSubService } from '../../services/index.js';
 import type { EventTopicBrokerPublishEventsForGoogle } from './broker-publish-events-google.js';
 
-async function createSource(
-  self: EventTopicBrokerPublishEventsForGoogle,
-  context: WorkspaceContext,
-): Promise<BackfillEventsSource> {
-  if (!self.source) {
-    const googleConf = context.asConfiguration<GoogleConfiguration>();
-    const projectId = googleConf.getOrThrow('google.project');
-    const rawEventsDatasetId = googleConf.get(
-      'google.pubSub.bigQueryStorage.rawEventsDatasetId',
-    );
-    if (!rawEventsDatasetId) {
-      throw new Error(
-        'Cannot use the default event source because BigQuery storage is not configured.',
-      );
-    }
+/**
+ * The number of events between each progress log line.
+ */
+const PUBLISH_PROGRESS_LOG_INTERVAL = 10000;
 
-    const tableName = self.eventTopic.replace(/[-\.]/g, '_');
-    const tableId = `${projectId}.${rawEventsDatasetId}.${tableName}`;
-    return new BigQueryEventsSource(context, tableId);
-  }
-
-  const bqSource = await BigQueryEventsSource.fromSourceAndFilter(
-    context,
-    self.source,
-    self.filter,
-  );
-  if (bqSource) {
-    return bqSource;
-  }
-
-  const jsonFilesSource = await JsonFilesEventSource.fromSourceAndFilter(
-    context,
-    self.source,
-    self.filter,
-  );
-  if (jsonFilesSource) {
-    return jsonFilesSource;
-  }
-
-  throw new Error(`The event source '${self.source}' is not supported.`);
-}
+/**
+ * Options when publishing events in batches to Pub/Sub.
+ */
+const PUBLISH_OPTIONS: PublishOptions = {
+  flowControlOptions: {
+    maxOutstandingBytes: 10 * 1024 * 1024,
+    maxOutstandingMessages: 1000,
+  },
+  batching: {
+    maxBytes: 10 * 1024 * 1024,
+    maxMessages: 1000,
+    maxMilliseconds: 1000,
+  },
+};
 
 export default async function call(
   this: EventTopicBrokerPublishEventsForGoogle,
   context: WorkspaceContext,
 ): Promise<void> {
-  const source = await createSource(this, context);
-  const publisher = new PubSubBackfillEventPublisher(context, this.topicId);
-  await publisher.publishFromSource(source);
+  const publisher: FlowControlledPublisher = context
+    .service(PubSubService)
+    .pubSub.topic(this.topicId, PUBLISH_OPTIONS)
+    .flowControlled();
+
+  context.logger.info('📫 Publishing events.');
+
+  let numEvents = 0;
+  for await (const event of this.source()) {
+    numEvents += 1;
+
+    const wait = publisher.publish({
+      data: event.data,
+      attributes: event.attributes,
+      orderingKey: event.key,
+    });
+    if (wait) {
+      context.logger.debug(
+        'Waiting for the publisher to catch up before resuming publishing.',
+      );
+      await wait;
+    }
+
+    if (numEvents % PUBLISH_PROGRESS_LOG_INTERVAL === 0) {
+      context.logger.debug(`Published ${numEvents} events.`);
+    }
+  }
+
+  await publisher.all();
+
+  context.logger.info(`📫 Finished publishing ${numEvents} events.`);
 }
