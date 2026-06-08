86f32d02a439e30d8bc056e5949d6e0a2f548bfa
Flo <florent.vouin@gmail.com>
2026-06-08 15:20:32 +0200

    ♻️ Adapt to Causa core breaking changes

--- src/functions/google-spanner/extract-database.spec.ts
+++ src/functions/google-spanner/extract-database.spec.ts
@@ -16,6 +16,7 @@ describe('ModelSchemaExtractDatabaseForGoogleSpanner', () => {
       name: 'MyTable',
       path: '/abs/my-table.yaml',
       properties: [],
+      additionalProperties: false,
       extensions,
       ...overrides,
     };
